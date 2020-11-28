ef64f0d138013197099fc730dd6cf87d070f1185
LiaoU3 <vincent932693@gmail.com>
2020-11-29 01:11:09 +0800

    update 2019/H/H-index

--- 2019/H/H-index_V4.py
+++ 2019/H/H-index_V4.py
@@ -1,5 +1,5 @@
 T = int(input())
-for x in range(1, T + 1):
+for case in range(1, T + 1):
     N = int(input())
     A = map(int, input().split())
     h = 0
@@ -8,14 +8,14 @@ for x in range(1, T + 1):
     new_citations = 0
     for A_i in A:
         if A_i > h:
-            try:
-                citations[A_i] += 1
-            except IndexError:
+            if A_i >= len(citations):
                 citations[N] += 1
+            else:
+                citations[A_i] += 1
             if new_citations == citations[h]:
                 h += 1
                 new_citations = 0
             else:
                 new_citations += 1
         y += ' ' + str(h)
-    print("Case #{}:{}".format(x, y), flush = True)
+    print("Case #{}:{}".format(case, y))
--- 2019/H/tset.py
+++
@@ -1,4 +0,0 @@
-import time
-for i in range(5):
-    time.sleep(1)
-    print(i, flush=False)
