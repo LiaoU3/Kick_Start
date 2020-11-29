ca0bdbbdf4437d70b94181067dea79ffa97b4568
LiaoU3 <vincent932693@gmail.com>
2020-11-29 14:52:24 +0800

    update H-index solution

--- 2019/H/H-index_V4.py
+++ 2019/H/H-index_V4.py
@@ -1,12 +1,12 @@
 T = int(input())
 for case in range(1, T + 1):
     N = int(input())
-    A = map(int, input().split())
+    As = list(map(int, input().split()))
     h = 0
-    y = ""
+    point = ""
     citations = [0] * (N + 1)
     new_citations = 0
-    for A_i in A:
+    for A_i in As:
         if A_i > h:
             if A_i >= len(citations):
                 citations[N] += 1
@@ -17,5 +17,5 @@ for case in range(1, T + 1):
                 new_citations = 0
             else:
                 new_citations += 1
-        y += ' ' + str(h)
-    print("Case #{}:{}".format(case, y))
+        point += ' ' + str(h)
+    print("Case #{}:{}".format(case, point))
